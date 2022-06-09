fc17d0be0801e83e4035f48efbb2b7329b18aad0
Vinayak Kukreja <78971045+vinayak-kukreja@users.noreply.github.com>
2022-06-09 07:05:56 -0700

    chore: repository created with projen (#1)

---
+++ .projenrc.js
@@ -0,0 +1,29 @@
+const { cdk8s } = require('projen');
+const project = new cdk8s.ConstructLibraryCdk8s({
+  name: 'cdk8s-jenkins',
+  packageName: 'cdk8s-jenkins',
+  description: 'Jenkins construct for CDK8s',
+  author: 'Amazon Web Services',
+  authorAddress: 'https://aws.amazon.com',
+  cdk8sVersion: '2.3.16',
+  defaultReleaseBranch: 'main',
+  repositoryUrl: 'git@github.com:cdk8s-team/cdk8s-jenkins.git',
+  peerDeps: [
+    'cdk8s',
+    'constructs',
+  ],
+  publishToMaven: {
+    javaPackage: 'org.cdk8s.jenkins',
+    mavenGroupId: 'org.cdk8s',
+    mavenArtifactId: 'cdk8s-jenkins',
+  },
+  publishToPypi: {
+    distName: 'cdk8s-jenkins',
+    module: 'cdk8s_jenkins',
+  },
+  publishToNuget: {
+    dotNetNamespace: 'Org.Cdk8s.Jenkins',
+    packageId: 'Org.Cdk8s.Jenkins',
+  },
+});
+project.synth();
