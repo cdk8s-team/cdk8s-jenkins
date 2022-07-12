530df3c4378c53f78e79b77c350ac300bcd17608
Vinayak Kukreja <78971045+vinayak-kukreja@users.noreply.github.com>
2022-07-11 18:07:53 -0700

    Update src/jenkins.ts
    Co-authored-by: Eli Polonsky <Eli.polonsky@gmail.com>
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- src/jenkins.ts
+++ src/jenkins.ts
@@ -9,7 +9,8 @@ import { DEFAULT_BASE_PLUGINS } from './utils/constants';
 export interface Plugin {
   /**
    * The url from where plugin has to be downloaded.
-   * By default the plugins are downloaded from Jenkins Update Centers. For more information, take a look { @link https://github.com/jenkinsci/kubernetes-operator/blob/master/pkg/configuration/base/resources/scripts_configmap.go#L121-L124 here }.
+   * @default - Plugins are downloaded from Jenkins Update Centers. 
+   * @see https://github.com/jenkinsci/kubernetes-operator/blob/master/pkg/configuration/base/resources/scripts_configmap.go#L121-L124
    */
   readonly downloadUrl?: string;
 
